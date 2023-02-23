d688a033c522bc04d795a63126dace5d93d3458f
Meenal-Sharma <meenal.sharma1@metacube.com>
2023-02-23 12:02:05 +0530

    implementing quote lifecycle

--- force-app/main/default/lwc/editQuote/editQuote.js
+++ force-app/main/default/lwc/editQuote/editQuote.js
@@ -3,14 +3,33 @@
  * Copyright (c) 2023 Provus Inc. All rights reserved.
  */
 
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, wire, track } from "lwc";
+import getQuoteDetails from '@salesforce/apex/QuoteController.getQuoteDetails';
+import { updateRecord } from "lightning/uiRecordApi";
 
 export default class EditQuote extends LightningElement {
   @api recordId;
   quoteData = {
-    name: "Quote Name",
-    endDate: 1547250828000
+    name: '',
+    startDate: '',
+    endDate: ''
   };
 
-  renderedCallback() {}
+  @wire(getQuoteDetails, {recordId: '$recordId'})
+  wiredQuote ({error, data}) {
+      if (error) {
+      } 
+      else if (data) {
+        console.log(data)
+        this.quoteData = data;
+      }
+  }
+
+  handleSave(){
+    const inputData = this.template.querySelectorAll('lightning-input');
+    
+  }
+
+  renderedCallback() {
+  }
 }
